bfb6c75b3eefccd3166571dd7bb6cbbee7dcaa0d
Hrithik Khadka <hritik.khadka@centria.fi>
2022-10-31 18:35:25 +0200

    fetch purchase orders

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ const url =
   "https://firebasestorage.googleapis.com/v0/b/gorgias-templates-production.appspot.com/o/attachments%2F3291ead6-82a2-43fa-ba6e-58328b4e02a1.json?alt=media&token=56fbe45a-2b91-4e35-bd1a-69f41f7d4363";
 
 function App() {
+  const [purchaseOrder, setPurchaseOrder] = useState();
+
+  const fetchPurchaseOrders = async () => {
+    try {
+      const response = await fetch(url);
+      const purchaseOrder = await response.json();
+      console.log(purchaseOrder);
+      setPurchaseOrder(purchaseOrder);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPurchaseOrders();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hi</h1>
