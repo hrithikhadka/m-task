e7775d40e93dbc4bca607a99aa51852a8d1fbb9f
Hrithik Khadka <hritik.khadka@centria.fi>
2022-11-01 15:52:23 +0200

    display purchase order type and number

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import { useState, useEffect } from "react";
 const url =
   "https://firebasestorage.googleapis.com/v0/b/gorgias-templates-production.appspot.com/o/attachments%2F3291ead6-82a2-43fa-ba6e-58328b4e02a1.json?alt=media&token=56fbe45a-2b91-4e35-bd1a-69f41f7d4363";
 
+const SingleOrder = ({
+  PurchaseOrderTypeAbbreviation,
+  PurchaseOrderNo,
+  PurchaseOrderId,
+  PurchaseOrderContactPerson,
+  PurchaseOrderStatus,
+  PurchaseOrderDetails,
+}) => {
+  return (
+    <div className="section">
+      <p>
+        {PurchaseOrderTypeAbbreviation} – {PurchaseOrderNo}
+      </p>
+    </div>
+  );
+};
+
 function App() {
   const [purchaseOrder, setPurchaseOrder] = useState();
 
@@ -11,8 +28,7 @@ function App() {
     try {
       const response = await fetch(url);
       const purchaseOrder = await response.json();
-      console.log(purchaseOrder);
-      setPurchaseOrder(purchaseOrder);
+      setPurchaseOrder(purchaseOrder.mvPurchaseOrders);
     } catch (error) {
       console.log(error);
     }
@@ -24,7 +40,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1>MVPurchase Orders</h1>
+      {purchaseOrder?.map((orders, index) => {
+        return <SingleOrder key={index} {...orders} />;
+      })}
     </div>
   );
 }
