c28312f441301420b42c860e0f75beb7d43b18d0
Hrithik Khadka <hritik.khadka@centria.fi>
2022-11-03 10:51:06 +0200

    add loading component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import SingleOrder from "./components/SingleOrder";
+import Loading from "./components/Loading";
 
 const url =
   "https://firebasestorage.googleapis.com/v0/b/gorgias-templates-production.appspot.com/o/attachments%2F3291ead6-82a2-43fa-ba6e-58328b4e02a1.json?alt=media&token=56fbe45a-2b91-4e35-bd1a-69f41f7d4363";
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [purchaseOrder, setPurchaseOrder] = useState();
 
   const fetchPurchaseOrders = async () => {
+    setLoading(true);
     try {
       const response = await fetch(url);
       const purchaseOrder = await response.json();
+      setLoading(false);
       setPurchaseOrder(purchaseOrder.mvPurchaseOrders);
     } catch (error) {
+      setLoading(false);
       console.log(error);
     }
   };
@@ -21,6 +26,10 @@ function App() {
   useEffect(() => {
     fetchPurchaseOrders();
   }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <div className="container">
